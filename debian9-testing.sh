cb2607df4a9692c38376d27029c2d3590cb3b30c
Km.Van <kmvan.com@gmail.com>
2019-02-19 23:30:41 +0800

    update to php7.3
    update to php7.3

--- debian9-testing.sh
+++ debian9-testing.sh
@@ -1,5 +1,4 @@
 #!/sbin/sh
 apt update && apt upgrade -y
-apt install software-properties-common python-pip php7.2-common php7.2-bcmath php7.2-fpm nginx php7.2-mbstring php7.2-sqlite3 php7.2-mysqli php7.2-redis php7.2-opcache php7.2-exif php7.2-imagick php7.2-gd php7.2-zip php7.2-curl imagemagick mariadb-server redis-server curl certbot screen vim htop iftop
-
+apt install software-properties-common python-pip	 php7.3-common php7.3-bcmath php7.3-fpm nginx php7.3-mbstring php7.3-sqlite3 php7.3-mysqli php7.3-redis php7.3-opcache php7.3-exif php7.3-imagick php7.3-gd php7.3-zip php7.3-curl imagemagick mariadb-server redis-server curl certbot screen vim htop iftop git php7.3-xml
 mysql_secure_installation
