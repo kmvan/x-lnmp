3acfb2dca72a9766aa1e021faeb963f0ceea1d10
Km.Van <kmvan.com@gmail.com>
2020-02-06 16:45:32 +0800

    add swoole installation

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-# add repository
+# Add repository
 add-apt-repository ppa:ondrej/php
 add-apt-repository ppa:ondrej/nginx
 
 # Update system
 apt update && apt upgrade -y
 
-# install PHP/Nginx/MySQL etc
+# Install PHP/Nginx/MySQL etc
 apt install certbot curl fish git htop iftop imagemagick mariadb-server neofetch nginx php7.4-bcmath php7.4-common php7.4-curl php7.4-exif php7.4-fpm php7.4-gd php7.4-imagick php7.4-mbstring php7.4-memcache php7.4-memcached php7.4-mysqli php7.4-opcache php7.4-redis php7.4-sqlite3 php7.4-xml php7.4-zip python-pip redis-server screen software-properties-common tldr vim
 
-# init MySQL/MariaDB
+# Install swoole
+apt install php7.4-dev php-pear
+pecl install swoole
+
+# Init MySQL/MariaDB
 mysql_secure_installation
 
 # PHP config: /etc/php/7.4/
