ab67e849eeec6786e452cb14e4f7e5d8e0ca2f9b
Km.Van <kmvan.com@gmail.com>
2020-02-06 15:13:02 +0800

    Update to php7.4

--- ubuntu.sh
+++ ubuntu.sh
@@ -8,7 +8,14 @@ add-apt-repository ppa:ondrej/nginx
 apt update && apt upgrade -y
 
 # install PHP/Nginx/MySQL etc
-apt install software-properties-common python-pip php7.3-common php7.3-bcmath php7.3-fpm nginx php7.3-mbstring php7.3-sqlite3 php7.3-mysqli php7.3-redis php7.3-opcache php7.3-exif php7.3-imagick php7.3-gd php7.3-zip php7.3-curl imagemagick mariadb-server redis-server curl certbot screen vim htop iftop git php7.3-xml php7.3-memcached php7.3-memcache tldr neofetch fish
+apt install certbot curl fish git htop iftop imagemagick mariadb-server neofetch nginx php7.4-bcmath php7.4-common php7.4-curl php7.4-exif php7.4-fpm php7.4-gd php7.4-imagick php7.4-mbstring php7.4-memcache php7.4-memcached php7.4-mysqli php7.4-opcache php7.4-redis php7.4-sqlite3 php7.4-xml php7.4-zip python-pip redis-server screen software-properties-common tldr vim
 
 # init MySQL/MariaDB
 mysql_secure_installation
+
+# PHP config: /etc/php/7.4/
+# Nginx config: /etc/nginx/
+# MySQL config: /etc/mysql/
+# Redis config: /etc/redis/redis.conf
+# Redis Database: /var/lib/redis/
+
