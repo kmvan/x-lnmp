c71d20d01ca5fbc7ee767325cc848a98bd1dda19
Km.Van <kmvan.com@gmail.com>
2020-02-21 13:45:13 +0800

    update to 7.4

--- arch.sh
+++ arch.sh
@@ -4,7 +4,7 @@
 pacman -Syu
 
 # install PHP/Nginx/MySQL etc
-pcamsn -Sy python-pip php7.3-common php7.3-bcmath php7.3-fpm nginx php7.3-mbstring php7.3-sqlite3 php7.3-mysqli php7.3-redis php7.3-opcache php7.3-exif php7.3-imagick php7.3-gd php7.3-zip php7.3-curl imagemagick mariadb-server redis-server curl certbot screen vim htop iftop git php7.3-xml php7.3-memcached php7.3-memcache tldr neofetch fish
+pcamsn -Sy python-pip php7.4-common php7.4-bcmath php7.4-fpm nginx php7.4-mbstring php7.4-sqlite4 php7.4-mysqli php7.4-redis php7.4-opcache php7.4-exif php7.4-imagick php7.4-gd php7.4-zip php7.4-curl imagemagick mariadb-server redis-server curl certbot screen vim htop iftop git php7.4-xml php7.4-memcached php7.4-memcache tldr neofetch fish
 
 # init MySQL/MariaDB
 mysql_secure_installation
