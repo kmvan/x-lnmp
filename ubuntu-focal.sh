107f0b4dc27290dab257ec43976736b679b050b8
Km.Van <kmvan.com@gmail.com>
2020-05-29 13:08:26 +0800

    add libwebp-dev
    add libwebp-dev

--- ubuntu-focal.sh
+++ ubuntu-focal.sh
@@ -4,7 +4,7 @@
 apt update && apt upgrade -y
 
 # Install PHP/Nginx/MySQL etc
-apt install certbot curl fish git htop iftop imagemagick mariadb-server neofetch nginx php7.4-bcmath php7.4-common php7.4-curl php7.4-exif php7.4-fpm php7.4-gd php-imagick php7.4-mbstring php-memcache php-memcached php7.4-mysql php7.4-opcache php-redis php7.4-sqlite3 php7.4-xml php7.4-zip redis-server screen software-properties-common tldr vim
+apt install certbot curl fish git htop iftop imagemagick mariadb-server neofetch nginx php7.4-bcmath php7.4-common php7.4-curl php7.4-exif php7.4-fpm php7.4-gd php-imagick php7.4-mbstring php-memcache php-memcached php7.4-mysql php7.4-opcache php-redis php7.4-sqlite3 php7.4-xml php7.4-zip redis-server screen software-properties-common tldr vim libwebp-dev
 
 # Install swoole
 apt install php7.4-dev php-pear
