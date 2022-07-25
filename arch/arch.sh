0b8e0527ac5a369c58b9f90253c2d8c8d915beda
Km.Van <kmvan.com@gmail.com>
2022-07-25 22:26:49 +0800

    Update arch.sh

--- arch/arch.sh
+++ arch/arch.sh
@@ -4,7 +4,8 @@
 pacman -Syu
 
 # install PHP/Nginx/MySQL etc
-pcamsn -Sy python-pip php7.4-common php7.4-bcmath php7.4-fpm nginx php7.4-mbstring php7.4-sqlite4 php7.4-mysqli php7.4-redis php7.4-opcache php7.4-exif php7.4-imagick php7.4-gd php7.4-zip php7.4-curl imagemagick mariadb-server redis-server curl certbot screen vim htop iftop git php7.4-xml php7.4-memcached php7.4-memcache tldr neofetch fish
+pcamsn -Sy nginx mariadb python-pip tldr neofetch fish redis curl certbot screen vim htop iftop git imagemagick\
+php-common php-bcmath php-fpm php-mbstring php-sqlite php-mysqli php-redis php-opcache php-exif php-imagick php-gd php-zip php-curl php-xml php-memcached php-mongodb php-grpc php-igbinary php-sodium
 
 # init MySQL/MariaDB
 mysql_secure_installation
