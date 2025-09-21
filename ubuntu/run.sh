e04f9a8a4e4ebacd44373eeb12ab614c1db16608
Km.Van <kmvan.com@gmail.com>
2025-09-21 21:15:41 +0800

    Update run.sh
    Remove unnecessary package

--- ubuntu/run.sh
+++ ubuntu/run.sh
@@ -10,7 +10,7 @@ sudo add-apt-repository ppa:ondrej/php
 sudo apt update && sudo apt upgrade -y
 
 # Install the software that may be required
-sudo apt install certbot curl fish git htop iftop imagemagick mariadb-server neofetch nginx redis-server screen tldr vim libwebp-dev
+sudo apt install certbot curl git htop iftop imagemagick mariadb-server nginx redis-server vim libwebp-dev
 
 # Install PHP
 sudo apt install php8.4-bcmath php8.4-common php8.4-curl php8.4-fpm php8.4-gd php8.4-imagick php8.4-mbstring php8.4-memcached php8.4-mysql php8.4-opcache php8.4-redis php8.4-sqlite3 php8.4-xml php8.4-zip
