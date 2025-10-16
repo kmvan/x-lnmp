825fa9e74174570255a2a92b782214064a60ebfc
Km.Van <kmvan.com@gmail.com>
2025-10-16 14:24:30 +0800

    Update run.sh

--- debian/run.sh
+++ debian/run.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-# Update system
-apt update && apt upgrade -y
+# To add this repository please do:
 
-# Install required dependencies
-apt install -y ca-certificates apt-transport-https software-properties-common
+if [ "$(whoami)" != "root" ]; then
+    SUDO=sudo
+fi
 
-# Use Surý APT repository
-wget -qO - https://packages.sury.org/php/apt.gpg | sudo apt-key add - 
-
-# Import repository key:
-echo "deb https://packages.sury.org/php/ bookworm main" | sudo tee /etc/apt/sources.list.d/php.list
-
-# Update
-apt update
+${SUDO} apt-get update
+${SUDO} apt-get -y install lsb-release ca-certificates curl
+${SUDO} curl -sSLo /tmp/debsuryorg-archive-keyring.deb https://packages.sury.org/debsuryorg-archive-keyring.deb
+${SUDO} dpkg -i /tmp/debsuryorg-archive-keyring.deb
+${SUDO} sh -c 'echo "deb [signed-by=/usr/share/keyrings/debsuryorg-archive-keyring.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
+${SUDO} apt-get update
 
 # install PHP/Nginx/MySQL etc
-apt install certbot curl fish git htop iftop imagemagick mariadb-server neofetch nginx redis-server screen tldr vim libwebp-dev
-apt install php8.3-bcmath php8.3-common php8.3-curl php8.3-fpm php8.3-gd php8.3-imagick php8.3-mbstring php8.3-memcached php8.3-mysql php8.3-opcache php8.3-redis php8.3-sqlite3 php8.3-xml php8.3-zip
+${SUDO} apt install certbot imagemagick mariadb-server nginx redis-server libwebp-dev
+${SUDO} apt install php8.4-bcmath php8.4-common php8.4-curl php8.4-fpm php8.4-gd php8.4-imagick php8.4-mbstring php8.4-memcached php8.4-mysql php8.4-opcache php8.4-redis php8.4-sqlite4 php8.4-xml php8.4-zip
 
 # init MySQL/MariaDB
-mysql_secure_installation
+${SUDO} mysql_secure_installation
