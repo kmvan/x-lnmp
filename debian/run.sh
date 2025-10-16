ae1a9e41e2457721e94cdd80374fa65e9abb41a1
Km.Van <kmvan.com@gmail.com>
2025-10-16 14:25:56 +0800

    Update SQLite package from sqlite4 to sqlite3

--- debian/run.sh
+++ debian/run.sh
@@ -15,7 +15,7 @@ ${SUDO} apt-get update
 
 # install PHP/Nginx/MySQL etc
 ${SUDO} apt install certbot imagemagick mariadb-server nginx redis-server libwebp-dev
-${SUDO} apt install php8.4-bcmath php8.4-common php8.4-curl php8.4-fpm php8.4-gd php8.4-imagick php8.4-mbstring php8.4-memcached php8.4-mysql php8.4-opcache php8.4-redis php8.4-sqlite4 php8.4-xml php8.4-zip
+${SUDO} apt install php8.4-bcmath php8.4-common php8.4-curl php8.4-fpm php8.4-gd php8.4-imagick php8.4-mbstring php8.4-memcached php8.4-mysql php8.4-opcache php8.4-redis php8.4-sqlite3 php8.4-xml php8.4-zip
 
 # init MySQL/MariaDB
 ${SUDO} mysql_secure_installation
